2874211b2fc6d9c0ee388d6bbcabfbe122dbb312
Thiago <115191028+thiago-deones@users.noreply.github.com>
2026-09-15 10:40:56 -0300

    criação do metodo boolean que confere se o email ja foi utilizado por outro aluno

--- 02-sistemaAcademia/src/main/java/dev/thiago/mastersys/repository/AlunoRepository.java
+++ 02-sistemaAcademia/src/main/java/dev/thiago/mastersys/repository/AlunoRepository.java
@@ -4,4 +4,5 @@ import dev.thiago.mastersys.domain.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    boolean existsByEmail(String email);
 }
