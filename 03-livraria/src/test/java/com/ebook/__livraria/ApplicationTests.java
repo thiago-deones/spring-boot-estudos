fc961045767876169393af2d4ad0e4580e2223a2
Thiago <115191028+thiago-deones@users.noreply.github.com>
2026-09-18 10:02:45 -0300

    iniciando projeto sistema livraria

---
+++ 03-livraria/src/test/java/com/ebook/__livraria/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebook.__livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
